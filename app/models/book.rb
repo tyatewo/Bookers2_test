a3f74d314930f69553ff68a349ef2de0ff8c94fb
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-07-07 07:11:55 +0000

    lange

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,9 +7,10 @@ class Book < ApplicationRecord
   validates :body,  presence: true, length: { maximum: 200 }
 
   #投稿数を日付別に表示する方法 scope :スコープの名前, -> { 条件式 }
-
+  #scopeとは、モデルに処理を定義することで、コントローラー等での記述を簡潔にする作業の事。イメージとしてはショートカットアイコンを作るような物でしょうか？
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日 ｛どこ（作成日：タイムゾーン．今.すべて）｝
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日 ｛どこ（作成日：タイムゾーン．昨日.すべて）｝
   scope :created_this_week, -> { where(created_at: Time.current.all_week) } # 今日 ｛どこ（作成日：タイムゾーン．今.すべて）｝
   scope :created_last_week, -> { where(created_at: Time.current.last_week.all_week) } # 前日 ｛どこ（作成日：タイムゾーン．昨日.すべて）｝
+  scope :created_day, ->(n) {where(created_at: n.days.ago.all_day)} #日付をnとして n.days.ago.all_day とすればn日前の日付が求められる
 end
