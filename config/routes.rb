2b52d48871aae5f55d7a0aeac22560106c685cad
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-07-07 08:11:54 +0000

    created_day(n).count

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    get 'search' => 'users#search'
   end
 
   resources :groups do
